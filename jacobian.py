66d4e0c711322da4a802914c9eeb0dfb111f6a2d
Bence Bardoczy <bardoczyb@gmail.com>
2019-07-01 13:55:23 -0500

    Sparse IdentityMatrix class to initialize Jacobians.

--- jacobian.py
+++ jacobian.py
@@ -1,9 +1,12 @@
 import numpy as np
+import copy
+import utils
+import simple_block as sim
 
 
 def chain_jacobians(jacdicts, inputs, T):
     """Obtain complete Jacobian of every output in jacdicts with respect to inputs, by applying chain rule."""
-    cumulative_jacdict = {i: {i: np.eye(T)} for i in inputs}
+    cumulative_jacdict = {i: {i: IdentityMatrix()} for i in inputs}
     for jacdict in jacdicts:
         cumulative_jacdict.update(compose_jacobians(cumulative_jacdict, jacdict))
     return cumulative_jacdict
@@ -25,7 +28,7 @@ def compose_jacobians(jacdict2, jacdict1):
 
 
 def apply_jacobians(jacdict, indict):
-    """Apply Jacobians in jacdict to indict to obtain outputs"""
+    """Apply Jacobians in jacdict to indict to obtain outputs."""
     outdict = {}
     for myout, innerjacdict in jacdict.items():
         for myin, jac in innerjacdict.items():
@@ -61,3 +64,89 @@ def unpack_jacobians(bigjac, inputs, outputs, T):
             jacdict[outputs[iO]][inputs[iI]] = bigjac[(T * iO):(T * (iO + 1)), (T * iI):(T * (iI + 1))]
     return jacdict
 
+
+def pack_sequences(sequences, names, T):
+    dX = np.zeros((T, len(names)))
+    for i, name in enumerate(names):
+        if name in sequences:
+            dX[:, i] = sequences[name]
+    return dX
+
+
+def unpack_sequences(dX, names):
+    sequences = {}
+    for i, name in enumerate(names):
+        sequences[name] = dX[:, i]
+    return sequences
+
+
+def pack_vectors(vs, names, T):
+    v = np.zeros(len(names)*T)
+    for i, name in enumerate(names):
+        if name in vs:
+            v[i*T:(i+1)*T] = vs[name]
+    return v
+
+
+def unpack_vectors(v, names, T):
+    vs = {}
+    for i, name in enumerate(names):
+        vs[name] = v[i*T:(i+1)*T]
+    return vs
+
+
+def make_matrix(A, T):
+    """
+    If A is not an outright ndarray, e.g. it is SimpleSparse, call its .matrix(T) method to convert it to T*T array.
+    """
+    if not isinstance(A, np.ndarray):
+        return A.matrix(T)
+    else:
+        return A
+
+
+class IdentityMatrix:
+    """Simple identity matrix class with which we can initialize chain_jacobians, avoiding costly explicit construction
+    of and operations on identity matrices."""
+    __array_priority__ = 10_000
+
+    def sparse(self):
+        """Equivalent SimpleSparse representation, less efficient operations but more general."""
+        return sim.SimpleSparse({(0, 0): 1})
+
+    def matrix(self, T):
+        return np.eye(T)
+
+    def __matmul__(self, other):
+        """Identity matrix knows to simply return 'other' whenever it's multiplied by 'other'."""
+        return copy.deepcopy(other)
+
+    def __rmatmul__(self, other):
+        return copy.deepcopy(other)
+
+    def __mul__(self, a):
+        return a*self.sparse()
+
+    def __rmul__(self, a):
+        return self.sparse()*a
+
+    def __add__(self, x):
+        return self.sparse() + x
+
+    def __radd__(self, x):
+        return x + self.sparse()
+
+    def __sub__(self, x):
+        return self.sparse() - x
+
+    def __rsub__(self, x):
+        return x - self.sparse()
+
+    def __neg__(self):
+        return -self.sparse()
+
+    def __pos__(self):
+        return self
+
+    def __repr__(self):
+        return 'IdentityMatrix'
