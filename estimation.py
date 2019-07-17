caf169d399d9486a22e558bc7b211a700743dd28
Matthew Rognlie <mrognlie@gmail.com>
2019-07-17 16:38:05 -0500

    documented and made more natural for user estimation.py, made krusell_smith notebook consistent with it

--- estimation.py
+++ estimation.py
@@ -2,34 +2,71 @@ import numpy as np
 import scipy.linalg as linalg
 from numba import njit
 
+'''Part 1: compute covariances at all lags and log likelihood'''
 
-def all_covariances(dX, sigmas, T):
-    """Use Fast Fourier Transform to compute covariance function between O vars up to T lags
+def all_covariances(M, sigmas):
+    """Use Fast Fourier Transform to compute covariance function between O vars up to T-1 lags.
 
     Parameters
     ----------
-    dX     : array (T*O*Z), stacked impulse responses of nO variables to nZ shocks
+    M      : array (T*O*Z), stacked impulse responses of nO variables to nZ shocks (MA(T-1) representation) 
     sigmas : array (Z), standard deviations of shocks
-    T      : int, truncation horizon
 
     Returns
     ----------
-    Sigma : array (T*O*O), covariance function between O variables for 0, ..., T lags
+    Sigma : array (T*O*O), covariance function between O variables for 0, ..., T-1 lags
     """
-    dft = np.fft.rfftn(dX, s=(2 * T - 2,), axes=(0,))
+    T = M.shape[0]
+    dft = np.fft.rfftn(M, s=(2 * T - 2,), axes=(0,))
     total = (dft.conjugate() * sigmas) @ dft.swapaxes(1, 2)
     return np.fft.irfftn(total, s=(2 * T - 2,), axes=(0,))[:T]
 
 
+def log_likelihood(Y, Sigma, sigma_measurement=None):
+    """Given second moments, compute log-likelihood of data Y.
+
+    Parameters
+    ----------
+    Y       : array (Tobs*O)
+                stacked data for O observables over Tobs periods
+    Sigma   : array (T*O*O)
+                covariance between observables in model for 0, ... , T lags (e.g. from all_covariances)
+    sigma_measurement : [optional] array (O)
+                            std of measurement error for each observable, assumed zero if not provided
+
+    Returns
+    ----------
+    L : scalar, log-likelihood
+    """
+    Tobs, nO = Y.shape
+    if sigma_measurement is None:
+        sigma_measurement = np.zeros(nO)
+    V = build_full_covariance_matrix(Sigma, sigma_measurement, Tobs)
+    y = Y.ravel()
+    return log_likelihood_formula(y, V)
+
+
+'''Part 2: helper functions'''
+
+def log_likelihood_formula(y, V):
+    """Implements multivariate normal log-likelihood formula using Cholesky with data vector y and variance V.
+       Calculates -log det(V)/2 - y'V^(-1)y/2
+    """
+    V_factored = linalg.cho_factor(V)
+    quadratic_form = np.dot(y, linalg.cho_solve(V_factored, y))
+    log_determinant = 2*np.sum(np.log(np.diag(V_factored[0])))
+    return -(log_determinant + quadratic_form) / 2
+
+
 @njit
-def build_full_covariance_matrix(Sigma, sigmas_measure, tau):
+def build_full_covariance_matrix(Sigma, sigma_measurement, Tobs):
     """Takes in T*O*O array Sigma with covariances at each lag t,
-    assembles them into (tau*O)*(tau*O) matrix of covariances, including measurement errors.
+    assembles them into (Tobs*O)*(Tobs*O) matrix of covariances, including measurement errors.
     """
     T, O, O = Sigma.shape
-    V = np.empty((tau, O, tau, O))
-    for t1 in range(tau):
-        for t2 in range(tau):
+    V = np.empty((Tobs, O, Tobs, O))
+    for t1 in range(Tobs):
+        for t2 in range(Tobs):
             if abs(t1-t2) >= T:
                 V[t1, :, t2, :] = np.zeros((O, O))
             else:
@@ -39,13 +76,5 @@ def build_full_covariance_matrix(Sigma, sigmas_measure, tau):
                     V[t1, : , t2, :] = Sigma[t1-t2, :, :].T
                 else:
                     # want exactly symmetric
-                    V[t1, :, t2, :] = (np.diag(sigmas_measure**2) + (Sigma[0, :, :]+Sigma[0, :, :].T)/2)
-    return V.reshape((tau*O, tau*O))
-
-
-def log_likelihood(V, w):
-    """Implements multivariate normal log-likelihood formula for observations w and variance V"""
-    V_factored = linalg.cho_factor(V)
-    quadratic_form = np.dot(w, linalg.cho_solve(V_factored, w))
-    log_determinant = 2*np.sum(np.log(np.diag(V_factored[0])))
-    return -(log_determinant + quadratic_form) / 2
+                    V[t1, :, t2, :] = (np.diag(sigma_measurement**2) + (Sigma[0, :, :]+Sigma[0, :, :].T)/2)
+    return V.reshape((Tobs*O, Tobs*O))
