c868b06953a0fafc6c96ac0736262a67843f40f8
Michael Cai <michaeldcai@gmail.com>
2020-07-13 16:35:57 -0500

    Implement basic ModelDAG class

--- sequence_jacobian/__init__.py
+++ sequence_jacobian/__init__.py
@@ -3,3 +3,4 @@
 from . import asymptotic, determinacy, estimation, jacobian, nonlinear, utils
 from .models import krusell_smith, hank, two_asset
 from .blocks import simple_block, het_block, solved_block
+from .model_dag import ModelDAG
---
+++ sequence_jacobian/model_dag.py
@@ -0,0 +1,16 @@
+"""The Directed Acyclic Graph (DAG) representation of a Model"""
+
+from typing import List
+
+
+class ModelDAG:
+    blocks: List  # Handle more elegantly later w/ a list of Block types, where simple/het/solved inherit from it
+    exogenous_variables: List[str]
+    unknown_variables: List[str]
+    targets: List[str]
+
+    def __init__(self, blocks, exogenous_variables, unknown_variables, targets):
+        self.blocks = blocks
+        self.exogenous_variables = exogenous_variables
+        self.unknown_variables = unknown_variables
+        self.targets = targets
