8556a6484cf9231cc873693d3715d9ec1d7da745
Michael Cai <michaeldcai@gmail.com>
2020-07-21 14:28:23 -0500

    Implement basic CombinedBlock class from the SHADE codebase

--- sequence_jacobian/__init__.py
+++ sequence_jacobian/__init__.py
@@ -5,4 +5,5 @@ from .models import krusell_smith, hank, two_asset
 from .blocks.simple_block import SimpleBlock, simple
 from .blocks.het_block import HetBlock, het
 from .blocks.helper_block import HelperBlock, helper
+from .blocks.combined_block import CombinedBlock, combine
 from .steady_state import steady_state
