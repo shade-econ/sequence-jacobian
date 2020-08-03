95108d29a961ff5d4aa041702e9ae85b54627cd4
Michael Cai <michaeldcai@gmail.com>
2020-08-03 14:24:00 -0500

    Test numerical outputs equal what they are supposed to with Ignore/IgnoreVector/Displace tests

--- tests/test_displacement_handlers.py
+++ tests/test_displacement_handlers.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 from sequence_jacobian.blocks.simple_block import (
-    Ignore, IgnoreVector, Displace, Reporter, Perturb, apply_op
+    Ignore, IgnoreVector, Displace, Reporter, Perturb, apply_op, numeric_primitive
 )
 
 
@@ -10,8 +10,8 @@ def test_ignore():
     # Test unary operations
     t1 = Ignore(1)
     for op in ["__neg__", "__pos__"]:
-        # TODO: Check that the primitive arithmetic is correct also
         assert type(apply_op(op, t1)) == Ignore
+        assert np.all(numeric_primitive(apply_op(op, t1)) == apply_op(op, numeric_primitive(t1)))
 
     # Test binary operations
     arg_pairs = [(Ignore(1), 1), (1, Ignore(1)), (Ignore(1), Ignore(2))]
@@ -19,16 +19,17 @@ def test_ignore():
         t1, t2 = pair
         for op in ["__add__", "__radd__", "__sub__", "__rsub__", "__mul__", "__rmul__",
                    "__truediv__", "__rtruediv__", "__pow__", "__rpow__"]:
-            # TODO: Check that the primitive arithmetic is correct also
             assert type(apply_op(op, t1, t2)) == Ignore
+            assert np.all(numeric_primitive(apply_op(op, t1, t2)) == apply_op(op, numeric_primitive(t1),
+                                                                              numeric_primitive(t2)))
 
 
 def test_ignore_vector():
     # Test unary operations
     t1 = IgnoreVector(np.array([1, 2, 3]))
     for op in ["__neg__", "__pos__"]:
-        # TODO: Check that the primitive arithmetic is correct also
         assert type(apply_op(op, t1)) == IgnoreVector
+        assert np.all(numeric_primitive(apply_op(op, t1)) == apply_op(op, numeric_primitive(t1)))
 
     # Test binary operations
     arg_pairs = [(IgnoreVector(np.array([1, 2, 3])), 1),
@@ -40,16 +41,17 @@ def test_ignore_vector():
         t1, t2 = pair
         for op in ["__add__", "__radd__", "__sub__", "__rsub__", "__mul__", "__rmul__",
                    "__truediv__", "__rtruediv__", "__pow__", "__rpow__"]:
-            # TODO: Check that the primitive arithmetic is correct also
             assert type(apply_op(op, t1, t2)) == IgnoreVector
+            assert np.all(numeric_primitive(apply_op(op, t1, t2)) == apply_op(op, numeric_primitive(t1),
+                                                                              numeric_primitive(t2)))
 
 
 def test_displace():
     # Test unary operations
     t1 = Displace(np.array([1, 2, 3]), ss=2)
     for op in ["__neg__", "__pos__"]:
-        # TODO: Check that the primitive arithmetic is correct also
         assert type(apply_op(op, t1)) == Displace
+        assert np.all(numeric_primitive(apply_op(op, t1)) == apply_op(op, numeric_primitive(t1)))
 
     # Test binary operations
     arg_pairs = [(Displace(np.array([1, 2, 3]), ss=2), 1),
@@ -63,5 +65,9 @@ def test_displace():
         t1, t2 = pair
         for op in ["__add__", "__radd__", "__sub__", "__rsub__", "__mul__", "__rmul__",
                    "__truediv__", "__rtruediv__", "__pow__", "__rpow__"]:
-            # TODO: Check that the primitive arithmetic is correct also
             assert type(apply_op(op, t1, t2)) == Displace
+            assert np.all(numeric_primitive(apply_op(op, t1, t2)) == apply_op(op, numeric_primitive(t1),
+                                                                              numeric_primitive(t2)))
+            assert np.all(numeric_primitive(apply_op(op, t1, t2).ss) ==\
+                   apply_op(op, t1.ss if isinstance(t1, Displace) else numeric_primitive(t1),
+                            t2.ss if isinstance(t2, Displace) else numeric_primitive(t2)))
