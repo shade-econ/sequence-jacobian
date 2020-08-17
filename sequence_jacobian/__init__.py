a655037706fe35a210fd4d40873ec7a3367bf0e3
Michael Cai <michaeldcai@gmail.com>
2020-08-17 14:21:56 -0500

    Change top-level exports from the public-facing API

--- sequence_jacobian/__init__.py
+++ sequence_jacobian/__init__.py
@@ -2,9 +2,10 @@
 
 from . import asymptotic, determinacy, estimation, jacobian, nonlinear, utils
 from .models import rbc, krusell_smith, hank, two_asset
-from .blocks.simple_block import SimpleBlock, simple, apply_function
-from .blocks.het_block import HetBlock, het
-from .blocks.helper_block import HelperBlock, helper
-from .blocks.solved_block import SolvedBlock, solved
-from .blocks.combined_block import CombinedBlock, combine
+from .blocks.simple_block import simple, apply_function
+from .blocks.het_block import het
+from .blocks.helper_block import helper
+from .blocks.solved_block import solved
+from .blocks.combined_block import combine
 from .steady_state import steady_state
+from .jacobian import get_G, get_H_U, get_impulse
