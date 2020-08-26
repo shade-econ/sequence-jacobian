6b4ea96d6409fdbebfbc798735c9cf8cc7665579
Michael Cai <michaeldcai@gmail.com>
2020-08-26 16:33:02 -0500

    Set up preliminary structure for SSJ primitives and base-level functionality for the package

---
+++ sequence_jacobian/base.py
@@ -0,0 +1,10 @@
+"""Type aliases, custom functions and errors for the base-level functionality of the package"""
+
+from typing import Any
+
+from .primitives import Block
+
+
+# Useful type aliases
+Array = Any
+BlockArray = Array[Block]
---
+++ sequence_jacobian/primitives.py
@@ -0,0 +1,50 @@
+"""Primitives to provide clarity and structure on blocks/models work"""
+
+import numpy as np
+import abc
+from numbers import Real
+from typing import Dict, Union, List
+
+from . import utilities as utils
+from .base import BlockArray
+
+
+# TODO: Refactor .ss, .td, and .jac methods for SimpleBlock and HetBlock to be cleaner so they can be interpreted from
+#   this more abstract representation of what canonical "Block"-like behavior should be
+class Block(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __init__(self, f):
+        self.f = f
+        self.inputs = set(utils.misc.input_list(f))
+        self.outputs = set(utils.misc.output_list(f))
+
+    @abc.abstractmethod
+    def ss(self, *ss_args, **ss_kwargs) -> Dict[str, Union[Real, np.ndarray]]:
+        """Call the block's function attribute `.f` on the pre-processed steady state (keyword) arguments,
+        ensuring that any time displacements will be ignored when `.f` is called.
+        See blocks.support.simple_displacement for an example of how SimpleBlocks do this pre-processing."""
+        return self.f(*ss_args, **ss_kwargs)
+
+    @abc.abstractmethod
+    def td(self, ss: Dict[str, Real], shock_paths: Dict[str, np.ndarray], **kwargs) -> Dict[str, np.ndarray]:
+        pass
+
+    @abc.abstractmethod
+    def jac(self, ss, shock_list, T):
+        pass
+
+
+class Model(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __init__(self, blocks: BlockArray, exogenous: List[str], unknowns: List[str], targets: List[str]) -> None:
+        self.blocks = blocks
+        self.exogenous = exogenous
+        self.unknowns = unknowns
+        self.targets = targets
+
+        # TODO: Implement standard checks, as in CombinedBlock in SHADE, for cyclic dependence, the right number of
+        #  unknowns and targets etc. to ensure that the model is well-defined.
