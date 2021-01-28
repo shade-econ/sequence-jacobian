26ff8c3e42da62b258575665da16b12b589fb092
Michael Cai <michaeldcai@gmail.com>
2021-01-27 21:02:46 -0600

    Implement a basic requirements.txt file

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 """Sets up the package."""
 
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# define a function that reads a file in this directory
+read = lambda p: Path(Path(__file__).resolve().parent / p).read_text()
+
 setup(
     name="sequence-jacobian",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=read("requirements.txt").splitlines(),
     version="0.0.1",
     author="Michael Cai",
     author_email="michaelcai@u.northwestern.edu",
@@ -14,11 +22,9 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shade-econ/sequence-jacobian",
-    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
