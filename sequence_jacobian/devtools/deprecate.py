383c1faae5d6f3be1253cc8afaff323a23ad3f50
Michael Cai <michaeldcai@gmail.com>
2021-03-16 17:20:57 -0500

    Deprecate `output_list` kwarg in favor of `outputs` in HetBlock

--- sequence_jacobian/devtools/deprecate.py
+++ sequence_jacobian/devtools/deprecate.py
@@ -8,3 +8,15 @@ import warnings
 
 # TODO: There are also the .ss, .td, and .jac methods that are deprecated within the various Block class definitions
 #   themselves.
+
+
+def rename_output_list_to_outputs(outputs=None, output_list=None):
+    if output_list is not None:
+        warnings.warn("The output_list kwarg has been deprecated and replaced with the outputs kwarg.",
+                      DeprecationWarning)
+        if outputs is None:
+            return output_list
+        else:
+            return list(set(outputs) | set(output_list))
+    else:
+        return outputs
