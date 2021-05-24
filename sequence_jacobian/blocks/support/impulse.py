546354004bbc4ab8b12b239cb6a3793bcb4f8304
bbardoczy <bardoczyb@gmail.com>
2021-05-24 14:25:41 -0500

    Arithmetic operations (+, -, *, /) for ImpulseDicts.
    Scope: numbers and SteadyStateDicts.

--- sequence_jacobian/blocks/support/impulse.py
+++ sequence_jacobian/blocks/support/impulse.py
@@ -6,16 +6,13 @@ from ...steady_state.classes import SteadyStateDict
 
 
 class ImpulseDict:
-    def __init__(self, impulse, ss):
+    def __init__(self, impulse):
         if isinstance(impulse, ImpulseDict):
             self.impulse = impulse.impulse
-            self.ss = impulse.ss
         else:
-            if not isinstance(impulse, dict) or not isinstance(ss, SteadyStateDict):
-                raise ValueError('ImpulseDicts are initialized with a `dict` of impulse responses'
-                                 ' and a `SteadyStateDict` of steady state values.')
+            if not isinstance(impulse, dict):
+                raise ValueError('ImpulseDicts are initialized with a `dict` of impulse responses.')
             self.impulse = impulse
-            self.ss = ss
 
     def __repr__(self):
         return f'<ImpulseDict: {list(self.impulse.keys())}>'
@@ -23,47 +20,70 @@ class ImpulseDict:
     def __iter__(self):
         return iter(self.impulse.items())
 
-    def __mul__(self, x):
-        return type(self)({k: x * v for k, v in self.impulse.items()}, self.ss)
-
-    def __rmul__(self, x):
-        return type(self)({k: x * v for k, v in self.impulse.items()}, self.ss)
-
     def __or__(self, other):
         if not isinstance(other, ImpulseDict):
             raise ValueError('Trying to merge an ImpulseDict with something else.')
-        if self.ss != other.ss:
-            raise ValueError('Trying to merge ImpulseDicts with different steady states.')
-        # make a copy, then add additional impulses
-        merged = type(self)(self.impulse, self.ss)
+        # Union returns a new ImpulseDict
+        merged = type(self)(self.impulse)
         merged.impulse.update(other.impulse)
         return merged
 
-    def __getitem__(self, x):
+    def __getitem__(self, item):
         # Behavior similar to pandas
-        if isinstance(x, str):
-            # case 1: ImpulseDict['C'] returns array
-            return self.impulse[x]
-        if isinstance(x, list):
-            # case 2: ImpulseDict[['C']] or ImpulseDict[['C', 'Y']] return smaller ImpulseDicts
-            return type(self)({k: self.impulse[k] for k in x}, self.ss)
+        if isinstance(item, str):
+            # Case 1: ImpulseDict['C'] returns array
+            return self.impulse[item]
+        if isinstance(item, list):
+            # Case 2: ImpulseDict[['C']] or ImpulseDict[['C', 'Y']] return smaller ImpulseDicts
+            return type(self)({k: self.impulse[k] for k in item})
 
-    def normalize(self, x=None):
-        if x is None:
-            # default: normalize by steady state if not zero
-            impulse = {k: v/self.ss[k] if not np.isclose(self.ss[k], 0) else v for k, v in self.impulse.items()}
-        else:
-            # normalize by steady state of x
-            if x not in self.ss.keys():
-                raise ValueError(f'Cannot normalize with {x}: steady state is unknown.')
-            elif np.isclose(self.ss[x], 0):
-                raise ValueError(f'Cannot normalize with {x}: steady state is zero.')
-            else:
-                impulse = {k: v / self.ss[x] for k, v in self.impulse.items()}
-        return type(self)(impulse, self.ss)
+    def __add__(self, other):
+        if isinstance(other, (float, int)):
+            return type(self)({k: v + other for k, v in self.impulse.items()})
+        if isinstance(other, SteadyStateDict):
+            return type(self)({k: v + other[k] for k, v in self.impulse.items()})
 
-    def levels(self):
-        return type(self)({k: v + self.ss[k] for k, v in self.impulse.items()}, self.ss)
+    def __sub__(self, other):
+        if isinstance(other, (float, int)):
+            return type(self)({k: v - other for k, v in self.impulse.items()})
+        if isinstance(other, SteadyStateDict):
+            return type(self)({k: v - other[k] for k, v in self.impulse.items()})
 
-    def deviations(self):
-        return type(self)({k: v - self.ss[k] for k, v in self.impulse.items()}, self.ss)
+    def __mul__(self, other):
+        if isinstance(other, (float, int)):
+            return type(self)({k: v * other for k, v in self.impulse.items()})
+        if isinstance(other, SteadyStateDict):
+            return type(self)({k: v * other[k] for k, v in self.impulse.items()})
+
+    def __rmul__(self, other):
+        if isinstance(other, (float, int)):
+            return type(self)({k: v * other for k, v in self.impulse.items()})
+        if isinstance(other, SteadyStateDict):
+            return type(self)({k: v * other[k] for k, v in self.impulse.items()})
+
+    def __truediv__(self, other):
+        if isinstance(other, (float, int)):
+            return type(self)({k: v / other for k, v in self.impulse.items()})
+        # ImpulseDict[['C, 'Y']] / ss[['C', 'Y']]: matches steady states; don't divide by zero
+        if isinstance(other, SteadyStateDict):
+            return type(self)({k: v / other[k] if not np.isclose(other[k], 0) else v for k, v in self.impulse.items()})
+
+    # def normalize(self, x=None):
+    #     if x is None:
+    #         # default: normalize by steady state if not zero
+    #         impulse = {k: v/self.ss[k] if not np.isclose(self.ss[k], 0) else v for k, v in self.impulse.items()}
+    #     else:
+    #         # normalize by steady state of x
+    #         if x not in self.ss.keys():
+    #             raise ValueError(f'Cannot normalize with {x}: steady state is unknown.')
+    #         elif np.isclose(self.ss[x], 0):
+    #             raise ValueError(f'Cannot normalize with {x}: steady state is zero.')
+    #         else:
+    #             impulse = {k: v / self.ss[x] for k, v in self.impulse.items()}
+    #     return type(self)(impulse, self.ss)
+    #
+    # def levels(self):
+    #     return type(self)({k: v + self.ss[k] for k, v in self.impulse.items()}, self.ss)
+    #
+    # def deviations(self):
+    #     return type(self)({k: v - self.ss[k] for k, v in self.impulse.items()}, self.ss)
