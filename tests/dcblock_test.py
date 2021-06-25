28c2987c5e250326e724695987945be6e71e1d3b
bbardoczy <bardoczyb@gmail.com>
2021-06-25 16:41:37 -0500

    Could .solve_steady_state return internals nested in CombinedBlocks?
    Can use remap_test.py to play with this.

--- tests/dcblock_test.py
+++ tests/dcblock_test.py
@@ -3,8 +3,8 @@
 import numpy as np
 
 from sequence_jacobian import simple, create_model, solved
-from single_endo import hh as hh_single
-from couple_endo import hh as hh_couple
+import single_endo as single
+import couple_endo as couple
 
 
 '''Rest of the model'''
@@ -101,39 +101,48 @@ def fiscal_ss(A, tax, w, Ze, rpost, Ui):
 '''Calibration'''
 
 cali_sm = {'beta': 0.9833, 'vphi': 0.7625, 'chi': 0.5585, 'fU': 0.2499, 'fN': 0.1148, 's': 0.0203,
-           'mean_z': 1.0, 'rho_z': 0.98, 'sd_z': 0.943*0.82, 'illiq': 0.25, 'pop': 0.29,
-           'fU_eps': 10.69, 'fN_eps': 5.57, 's_eps': -11.17}
+           'mean_z': 1.0, 'rho_z': 0.98, 'sd_z': 0.943*0.82,
+           'fU_eps': 10.69, 'fN_eps': 5.57, 's_eps': -11.17,
+           'illiq': 0.25, 'pop': 0.29}
 
 cali_sw = {'beta': 0.9830, 'vphi': 0.9940, 'chi': 0.4958, 'fU': 0.2207, 'fN': 0.1098, 's': 0.0132,
-           'mean_z': 0.8, 'rho_z': 0.98, 'sd_z': 0.86*0.82, 'illiq': 0.15, 'pop': 0.29,
-           'fU_eps': 8.72, 'fN_eps': 3.55, 's_eps': -6.55}
+           'mean_z': 0.8, 'rho_z': 0.98, 'sd_z': 0.86*0.82,
+           'fU_eps': 8.72, 'fN_eps': 3.55, 's_eps': -6.55,
+           'illiq': 0.15, 'pop': 0.29}
 
-cali_mc = {'beta': 0.9882, 'illiq': 0.6, 'pop': 0.42, 'rho': 0.042,
+cali_mc = {'beta': 0.9882, 'rho': 0.042,
            'vphi_m': 0.1545, 'chi_m': 0.2119, 'fU_m': 0.3013, 'fN_m': 0.1840, 's_m': 0.0107,
            'vphi_f': 0.2605, 'chi_f': 0.2477, 'fU_f': 0.2519, 'fN_f': 0.1691, 's_f': 0.0103,
            'mean_m': 1.0, 'rho_m': 0.98, 'sd_m': 0.943*0.82,
            'mean_f': 0.8, 'rho_f': 0.98, 'sd_f': 0.86*0.82,
            'fU_eps_m': 10.37, 'fN_eps_m': 9.74, 's_eps_m': -13.60,
-           'fU_eps_f': 8.40, 'fN_eps_f': 2.30, 's_eps_f': -7.87}
-
+           'fU_eps_f': 8.40, 'fN_eps_f': 2.30, 's_eps_f': -7.87,
+           'illiq': 0.6, 'pop': 0.42}
 
 '''Remap'''
 
-
+# variables to remap
 to_map_single = ['beta', 'vphi', 'chi', 'fU', 'fN', 's', 'mean_z', 'rho_z', 'sd_z', 'transfer',
-                 'fU_eps', 'fN_eps', 's_eps', *hh_single.outputs]
-hh_sm = hh_single.remap({k: k + '_sm' for k in to_map_single}).rename('SingleMen')
-hh_sw = hh_single.remap({k: k + '_sw' for k in to_map_single}).rename('SingleWomen')
-
+                 'fU_eps', 'fN_eps', 's_eps', *single.hh.outputs]
 to_map_couple = ['beta', 'transfer', 'rho',
                  'vphi_m', 'chi_m', 'fU_m', 'fN_m', 's_m', 'mean_m', 'rho_m', 'sd_m', 'fU_eps_m', 'fN_eps_m', 's_eps_m',
                  'vphi_f', 'chi_f', 'fU_f', 'fN_f', 's_f', 'mean_f', 'rho_f', 'sd_f', 'fU_eps_f', 'fN_eps_f', 's_eps_f',
-                 *hh_couple.outputs]
-hh_mc = hh_couple.remap({k: k + '_mc' for k in to_map_couple}).rename('Couples')
+                 *couple.hh.outputs]
 
-cali_sm = {k + '_sm': v for k, v in cali_sm.items()}
-cali_sw = {k + '_sw': v for k, v in cali_sw.items()}
-cali_mc = {k + '_mc': v for k, v in cali_mc.items()}
+# Single men
+hh_sm = create_model([single.income_state_vars, single.employment_state_vars, single.asset_state_vars, single.flows,
+                      single.household.rename('single_men')], name='SingleMen')
+hh_sm = hh_sm.remap({k: k + '_sm' for k in to_map_single})
+
+# Single women
+hh_sw = create_model([single.income_state_vars, single.employment_state_vars, single.asset_state_vars, single.flows,
+                      single.household.rename('single_women')], name='SingleWomen')
+hh_sw = hh_sw.remap({k: k + '_sw' for k in to_map_single})
+
+# Married couples
+hh_mc = create_model([couple.income_state_vars, couple.employment_state_vars, couple.asset_state_vars,
+                      couple.flows_m, couple.flows_f, couple.household.rename('couples')], name='Couples')
+hh_mc = hh_mc.remap({k: k + '_mc' for k in to_map_couple})
 
 
 '''Solve ss'''
@@ -141,6 +150,11 @@ cali_mc = {k + '_mc': v for k, v in cali_mc.items()}
 
 hank = create_model([hh_sm, hh_sw, hh_mc, aggregate, dividends, firm, monetary, valuation, nkpc, fiscal, mkt_clearing],
                     name='HANK')
+
+# remap calibration
+cali_sm = {k + '_sm': v for k, v in cali_sm.items()}
+cali_sw = {k + '_sw': v for k, v in cali_sw.items()}
+cali_mc = {k + '_mc': v for k, v in cali_mc.items()}
 
 calibration = {**cali_sm, **cali_sw, **cali_mc,
                'eis': 1.0, 'uicap': 0.66, 'uirate': 0.5, 'expiry': 1/6, 'eps': 10.0, 'tax': 0.3,
@@ -154,4 +168,12 @@ ss = hank.solve_steady_state(calibration, solver='toms748', dissolve=[fiscal], t
                                       'Y': 1.0, 'nkpc_res': 0.0, 'budget': 0.0, 'asset_mkt': 0.0, 'val': 0.0, 'u': 1.0},
                              helper_blocks=[firm_ss, fiscal_ss],
                              helper_targets=['asset_mkt', 'budget', 'val', 'nkpc_res', 'Y', 'u'])
+
+
+# jacobians
+# J = {}
+# J['sm'] = hh_sm.jacobian(ss, exogenous=['atw', 'rpost', 'beta_sm', 'Y', 'transfer_sm'], T=500)
+
+# td_nonlin = hank.solve_impulse_nonlinear(ss, {'Z': 0.001*0.9**np.arange(300)},
+#                                          unknowns=['K'], targets=['asset_mkt'])
 
