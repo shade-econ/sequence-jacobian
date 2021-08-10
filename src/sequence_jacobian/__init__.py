5098333e977448a9909151a3e51e0d7a53d2f8db
Matthew Rognlie <matthew.rognlie@northwestern.edu>
2021-08-10 17:21:21 -0500

    minor changes so pytest can run, one error thrown because JacobianDictBlock does not have a name?

--- src/sequence_jacobian/__init__.py
+++ src/sequence_jacobian/__init__.py
@@ -2,7 +2,7 @@
 
 from . import estimation, jacobian, nonlinear, utilities, devtools
 
-#from .models import rbc, krusell_smith, hank, two_asset
+from .models import rbc, krusell_smith, hank, two_asset
 
 from .blocks.simple_block import simple
 from .blocks.het_block import het, hetoutput
