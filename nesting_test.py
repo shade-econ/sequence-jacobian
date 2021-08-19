Refactoring of .impulse_linear is almost done. Status: hetoutput caused some trouble, should be easy to fix analogously to .jacobian.

nesting_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import sequence_jacobian as sj
 from sequence_jacobian import het, simple, hetoutput, combine, solved, create_model, get_H_U
 from sequence_jacobian.jacobian.classes import JacobianDict, ZeroMatrix
+from sequence_jacobian.blocks.support.impulse import ImpulseDict
 
 
 '''Part 1: Household block'''
@@ -142,24 +143,24 @@ calibration = {'Y': 1.0, 'r': 0.005, 'sigma': 2.0, 'rho_e': 0.91, 'sd_e': 0.92, 
                'amin': 0.0, 'amax': 1000, 'nA': 100, 'Gamma': 0.0, 'transfer': 0.143}
 calibration['rho_B'] = 0.8
 
-ss0 = dag.solve_steady_state(calibration, solver='hybr',
-                            unknowns={'beta': .95, 'G': 0.2, 'B': 2.0},
-                            targets={'asset_mkt': 0.0, 'tau': 0.334, 'Mpc': 0.25})
+# ss0 = dag.solve_steady_state(calibration, solver='hybr',
+#                             unknowns={'beta': .95, 'G': 0.2, 'B': 2.0},
+#                             targets={'asset_mkt': 0.0, 'tau': 0.334, 'Mpc': 0.25})
 
 #Js = dag.partial_jacobians(ss0, inputs=['Y', 'r'], T=10)
 
-# @solved(unknowns={'B': (0.0, 10.0)}, targets=['B_rule'], solver='brentq')
-# def fiscal_solved(B, G, rb, Y, transfer, rho_B):
-#     B_rule = B.ss + rho_B * (B(-1) - B.ss + G - G.ss) - B
-#     rev = (1 + rb) * B(-1) + G + transfer - B   # revenue to be raised
-#     tau = rev / Y
-#     return B_rule, rev, tau
+@solved(unknowns={'B': (0.0, 10.0)}, targets=['B_rule'], solver='brentq')
+def fiscal_solved(B, G, rb, Y, transfer, rho_B):
+    B_rule = B.ss + rho_B * (B(-1) - B.ss + G - G.ss) - B
+    rev = (1 + rb) * B(-1) + G + transfer - B   # revenue to be raised
+    tau = rev / Y
+    return B_rule, rev, tau
 
-# dag = sj.create_model([hh, interest_rates, fiscal_solved, mkt_clearing], name='HANK')
+dag = sj.create_model([hh, interest_rates, fiscal_solved, mkt_clearing], name='HANK')
 
-# ss = dag.solve_steady_state(calibration, dissolve=['fiscal_solved'], solver='hybr',
-#                             unknowns={'beta': .95, 'G': 0.2, 'B': 2.0},
-#                             targets={'asset_mkt': 0.0, 'tau': 0.334, 'Mpc': 0.25})
+ss = dag.solve_steady_state(calibration, dissolve=['fiscal_solved'], solver='hybr',
+                            unknowns={'beta': .95, 'G': 0.2, 'B': 2.0},
+                            targets={'asset_mkt': 0.0, 'tau': 0.334, 'Mpc': 0.25})
 
 # assert all(np.allclose(ss0[k], ss[k]) for k in ss0)
 
@@ -198,10 +199,17 @@ T = 5
 # J_all2 = J_int.compose(J_hh)
 
 
-G = dag.solve_jacobian(ss0, inputs=['r'], outputs=['A', 'Y', 'asset_mkt', 'goods_mkt'], unknowns=['Y', 'B'], targets=['asset_mkt', 'B_rule'], T=300)
+# G = dag.solve_jacobian(ss0, inputs=['r'], outputs=['A', 'Y', 'asset_mkt', 'goods_mkt'], unknowns=['Y', 'B'], targets=['asset_mkt', 'B_rule'], T=300)
 
-# td_lin = dag.solve_impulse_linear(ss, {'r': 0.001*0.9**np.arange(300)},
-#                                   unknowns=['B', 'Y'], targets=['asset_mkt', 'B_rule'])
+G = dag.solve_jacobian(ss, inputs=['r'], outputs=['A', 'Y', 'asset_mkt', 'goods_mkt'], unknowns=['Y'], targets=['asset_mkt'], T=300)
+
+shock = ImpulseDict({'r': 0.001*0.9**np.arange(300)})
+
+td1 = G @ shock 
+
+
+td_lin = dag.solve_impulse_linear(ss, unknowns=['Y'], targets=['asset_mkt'],
+                                  inputs=shock, outputs=['A', 'Y', 'asset_mkt', 'goods_mkt'])
 
 # td_nonlin = dag.solve_impulse_nonlinear(ss, {'r': 0.001*0.9**np.arange(300)},
 #                                         unknowns=['B', 'Y'], targets=['asset_mkt', 'B_rule'])

src/sequence_jacobian/blocks/support/impulse.py
@@ -20,21 +20,17 @@ class ImpulseDict:
                 T = self.infer_length()
             self.T = T
 
-    def keys(self):
-        return self.impulse.keys()
-
-    def pack(self):
-        T = self.T
-        bigv = np.empty(T*len(self.impulse))
-        for i, v in enumerate(self.impulse.values()):
-            bigv[i*T:(i+1)*T] = v
-        return bigv
-
     def __repr__(self):
         return f'<ImpulseDict: {list(self.impulse.keys())}>'
 
     def __iter__(self):
         return iter(self.impulse)
+
+    def items(self):
+        return self.impulse.items()
+
+    def update(self, other):
+        return self.impulse.update(other.impulse)
 
     def __or__(self, other):
         if not isinstance(other, ImpulseDict):
@@ -108,9 +104,26 @@ class ImpulseDict:
     def __rmatmul__(self, x):
         return self.__matmul__(x)
 
+    def keys(self):
+        return self.impulse.keys()
+
+    def pack(self):
+        T = self.T
+        bigv = np.empty(T*len(self.impulse))
+        for i, v in enumerate(self.impulse.values()):
+            bigv[i*T:(i+1)*T] = v
+        return bigv
+
+    @staticmethod
+    def unpack(bigv, outputs, T):
+        impulse = {}
+        for i, o in enumerate(outputs):
+            impulse[o] = bigv[i*T:(i+1)*T]
+        return ImpulseDict(impulse)
+
     def infer_length(self):
         lengths = [len(v) for v in self.impulse.values()]
         length = max(lengths)
-        if lengths != min(lengths):
+        if length != min(lengths):
             raise ValueError(f'Building ImpulseDict with inconsistent lengths {max(lengths)} and {min(lengths)}')
         return length

src/sequence_jacobian/jacobian/classes.py
@@ -5,14 +5,15 @@ import copy
 import warnings
 import numpy as np
 
-from sequence_jacobian.primitives import Array
-
 from . import support
 from ..utilities.misc import factor, factored_solve
 from ..utilities.ordered_set import OrderedSet
 from ..blocks.support.bijection import Bijection
 from ..blocks.support.impulse import ImpulseDict
-from typing import Dict, Union
+from typing import Any, Dict, Union
+
+# Basic types
+Array = Any
 
 
 class Jacobian(metaclass=ABCMeta):
@@ -401,16 +402,21 @@ class JacobianDict(NestedDict):
         if isinstance(x, JacobianDict):
             return self.compose(x)
         elif isinstance(x, Bijection):
-            nesteddict = x @ self.nesteddict
-            for o in nesteddict.keys():
-                nesteddict[o] = x @ nesteddict[o]
-            return JacobianDict(nesteddict, inputs=x @ self.inputs, outputs=x @ self.outputs)
+            return self.remap(x)
         else:
             return self.apply(x)
 
     def __rmatmul__(self, x):
         if isinstance(x, Bijection):
-            return JacobianDict(x @ self.nesteddict, inputs=x @ self.inputs, outputs=x @ self.outputs)
+            return self.remap(x)
+
+    def remap(self, x: Bijection):
+        if not x:
+            return self
+        nesteddict = x @ self.nesteddict
+        for o in nesteddict.keys():
+            nesteddict[o] = x @ nesteddict[o]
+        return JacobianDict(nesteddict, inputs=x @ self.inputs, outputs=x @ self.outputs)
 
     def __bool__(self):
         return bool(self.outputs) and bool(self.inputs)
@@ -479,7 +485,7 @@ class JacobianDict(NestedDict):
                 jacdict[O][I] = bigjac[(T * iO):(T * (iO + 1)), (T * iI):(T * (iI + 1))]
         return JacobianDict(jacdict, outputs, inputs, T=T)
 
-class FactoredJacobianDict(JacobianDict):
+class FactoredJacobianDict:
     def __init__(self, jacobian_dict: JacobianDict, T=None):
         if jacobian_dict.T is None:
             if T is None:
@@ -498,6 +504,30 @@ class FactoredJacobianDict(JacobianDict):
 
     def __repr__(self):
         return f'<{type(self).__name__} unknowns={self.unknowns}, targets={self.targets}>'
+
+    # TODO: test this
+    def to_jacobian_dict(self):
+        return JacobianDict.unpack(-factored_solve(self.H_U_factored, np.eye(self.T*len(self.unknowns))), self.unknowns, self.targets, self.T)
+
+    def __matmul__(self, x):
+        if isinstance(x, JacobianDict):
+            return self.compose(x)
+        elif isinstance(x, Bijection):
+            return self.remap(x)
+        else:
+            return self.apply(x)
+
+    def __rmatmul__(self, x):
+        if isinstance(x, Bijection):
+            return self.remap(x)
+
+    def remap(self, x: Bijection):
+        if not x:
+            return self
+        newself = copy.copy(self)
+        newself.unknowns = x @ self.unknowns
+        newself.targets = x @ self.targets
+        return newself
 
     def compose(self, J: JacobianDict):
         # take intersection of J outputs with self.targets
@@ -580,4 +610,3 @@ def verify_saved_jacobian(block_name, Js, outputs, inputs, T):
             return False
 
     return True
-

src/sequence_jacobian/primitives.py
@@ -11,8 +11,8 @@ from sequence_jacobian.utilities.ordered_set import OrderedSet
 
 from .steady_state.drivers import steady_state as ss
 from .steady_state.support import provide_solver_default
-from .nonlinear import td_solve
-from .jacobian.drivers import get_impulse, get_G
+# from .nonlinear import td_solve
+# from .jacobian.drivers import get_impulse, get_G
 from .steady_state.classes import SteadyStateDict, UserProvidedSS
 from .jacobian.classes import JacobianDict
 from .blocks.support.impulse import ImpulseDict
@@ -96,10 +96,13 @@ class Block(abc.ABC, metaclass=ABCMeta):
         from a steady state `ss`."""
         return self.M @ self._impulse_nonlinear(self.M.inv @ ss, self.M.inv @ exogenous, **kwargs)
 
-    def impulse_linear(self, ss: SteadyStateDict, inputs: Union[Dict[str, Array], ImpulseDict], outputs: Optional[List[str]] = None, Js={}) -> ImpulseDict:
-        """Calculate a partial equilibrium, linear impulse response to a set of `exogenous` shocks
-        from a steady state `ss`."""
-        return self.M @ self._impulse_linear(self.M.inv @ ss, self.M.inv @ exogenous, **kwargs)
+    def impulse_linear(self, ss: SteadyStateDict, inputs: Union[Dict[str, Array], ImpulseDict],
+                       outputs: Optional[List[str]] = None, Js={}) -> ImpulseDict:
+        """Calculate a partial equilibrium, linear impulse response of `outputs` to a set of shocks in `inputs`
+        around a steady state `ss`."""
+        inputs = ImpulseDict(inputs)
+        _, outputs = self.default_inputs_outputs(ss, inputs.keys(), outputs)
+        return self.M @ self._impulse_linear(self.M.inv @ ss, self.M.inv @ inputs, self.M.inv @ outputs, Js)
 
     def partial_jacobians(self, ss, inputs=None, outputs=None, T=None, Js={}):
         # TODO: annotate signature
@@ -107,11 +110,11 @@ class Block(abc.ABC, metaclass=ABCMeta):
             inputs = self.inputs
         if outputs is None:
             outputs = self.outputs
-        inputs, outputs = set(inputs), set(outputs)
+        inputs, outputs = inputs, outputs
         
         # if you have a J for this block that already has everything you need, use it
         # TODO: add check for T,  maybe look at verify_saved_jacobian for ideas?
-        if (self.name in Js) and (inputs <= Js[self.name].inputs) and (outputs <= Js[self.name].outputs):
+        if (self.name in Js) and isinstance(Js[self.name], JacobianDict) and (inputs <= Js[self.name].inputs) and (outputs <= Js[self.name].outputs):
             return {self.name: Js[self.name][outputs, inputs]}
 
         # if it's a leaf, just call Jacobian method, include if nonzero
@@ -129,10 +132,9 @@ class Block(abc.ABC, metaclass=ABCMeta):
                  T: Optional[int] = None, Js={}) -> JacobianDict:
         """Calculate a partial equilibrium Jacobian to a set of `input` shocks at a steady state `ss`."""
         inputs, outputs = self.default_inputs_outputs(ss, inputs, outputs)
-        inputs, outputs = OrderedSet(inputs), OrderedSet(outputs)
 
         # if you have a J for this block that has everything you need, use it
-        if (self.name in Js) and (inputs <= Js[self.name].inputs) and (outputs <= Js[self.name].outputs):
+        if (self.name in Js) and isinstance(Js[self.name], JacobianDict) and (inputs <= Js[self.name].inputs) and (outputs <= Js[self.name].outputs):
             return Js[self.name][outputs, inputs]
         
         # if it's a leaf, call Jacobian method, don't supply Js
@@ -171,23 +173,27 @@ class Block(abc.ABC, metaclass=ABCMeta):
                                      unknowns=unknowns, targets=targets, Js=Js, **kwargs)
         return ImpulseDict(irf_nonlin_gen_eq)
 
-    def solve_impulse_linear(self, ss: Dict[str, Union[Real, Array]],
-                             exogenous: Dict[str, Array],
-                             unknowns: List[str], targets: List[str],
-                             T: Optional[int] = None,
-                             Js: Optional[Dict[str, JacobianDict]] = {},
-                             **kwargs) -> ImpulseDict:
-        """Calculate a general equilibrium, linear impulse response to a set of `exogenous` shocks
-        from a steady state `ss`, given a set of `unknowns` and `targets` corresponding to the endogenous
+    def solve_impulse_linear(self, ss: SteadyStateDict, unknowns: List[str], targets: List[str],
+                             inputs: Union[Dict[str, Array], ImpulseDict], outputs: Optional[List[str]],
+                             Js: Optional[Dict[str, JacobianDict]] = {}) -> ImpulseDict:
+        """Calculate a general equilibrium, linear impulse response to a set of shocks in `inputs` around a steady state `ss`, given a set of `unknowns` and `targets` corresponding to the endogenous
         variables to be solved for and the target conditions that must hold in general equilibrium"""
-        blocks = self.blocks if hasattr(self, "blocks") else [self]
-        irf_lin_gen_eq = get_impulse(blocks, exogenous, unknowns, targets, T=T, ss=ss, Js=Js, **kwargs)
-        return ImpulseDict(irf_lin_gen_eq)
+        inputs = ImpulseDict(inputs)
+        input_names, outputs = self.default_inputs_outputs(ss, inputs.keys(), outputs)
+        unknowns, targets = OrderedSet(unknowns), OrderedSet(targets)
 
-    def solve_jacobian(self, ss: Dict[str, Union[Real, Array]], unknowns: List[str], targets: List[str],
+        Js = self.partial_jacobians(ss, input_names | unknowns, (outputs | targets) - unknowns, inputs.T, Js)
+
+        H_U = self.jacobian(ss, unknowns, targets, inputs.T, Js).pack(inputs.T)
+        dH = self.impulse_linear(ss, inputs, targets, Js).pack()
+        dU = ImpulseDict.unpack(-np.linalg.solve(H_U, dH), unknowns, inputs.T)
+
+        return self.impulse_linear(ss, dU | inputs, outputs - unknowns, Js)
+
+
+    def solve_jacobian(self, ss: SteadyStateDict, unknowns: List[str], targets: List[str],
                        inputs: List[str], outputs: Optional[List[str]] = None, T: Optional[int] = None,
-                       Js: Optional[Dict[str, JacobianDict]] = {},
-                       **kwargs) -> JacobianDict:
+                       Js: Optional[Dict[str, JacobianDict]] = {}) -> JacobianDict:
         """Calculate a general equilibrium Jacobian to a set of `exogenous` shocks
         at a steady state `ss`, given a set of `unknowns` and `targets` corresponding to the endogenous
         variables to be solved for and the target conditions that must hold in general equilibrium"""
@@ -196,9 +202,9 @@ class Block(abc.ABC, metaclass=ABCMeta):
             T = 300
 
         inputs, outputs = self.default_inputs_outputs(ss, inputs, outputs)
-        inputs, unknowns, targets = list(inputs), list(unknowns), list(targets)
+        unknowns, targets = OrderedSet(unknowns), OrderedSet(targets)
 
-        Js = self.partial_jacobians(ss, set(inputs) | set(unknowns), (set(outputs) | set(targets)) - set(unknowns), T, Js)
+        Js = self.partial_jacobians(ss, inputs | unknowns, (outputs | targets) - unknowns, T, Js)
         
         H_U = self.jacobian(ss, unknowns, targets, T, Js).pack(T)
         H_Z = self.jacobian(ss, inputs, targets, T, Js).pack(T)
@@ -206,7 +212,7 @@ class Block(abc.ABC, metaclass=ABCMeta):
 
         from . import combine
         self_with_unknowns = combine([U_Z, self])
-        return self_with_unknowns.jacobian(ss, inputs, set(unknowns) | set(outputs), T, Js)
+        return self_with_unknowns.jacobian(ss, inputs, unknowns | outputs, T, Js)
 
     def solved(self, unknowns, targets, name=None, solver=None, solver_kwargs=None):
         if name is None:
@@ -240,4 +246,4 @@ class Block(abc.ABC, metaclass=ABCMeta):
             inputs = self.inputs
         if outputs is None:
             outputs = self.outputs - ss._vector_valued()
-        return inputs, outputs
+        return OrderedSet(inputs), OrderedSet(outputs)