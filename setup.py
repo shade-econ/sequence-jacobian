3a70e3caf43a1c5e5364298b9b6c6b39240c18da
bbardoczy <bardoczyb@gmail.com>
2022-01-04 19:35:48 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,9 @@ read = lambda p: Path(Path(__file__).resolve().parent / p).read_text()
 
 setup(
     name="sequence-jacobian",
-    python_requires=">=3.7",
-    install_requires=read("requirements.txt").splitlines(),
     version="1.0.0-alpha",
-    author="Matthew Rognlie, Bence Bardoczy, Michael Cai",
-    author_email="matthew.rognlie@northwestern.edu",
+    author="Sequence-Jacobian Team",
+    author_email="SequenceJacobianTeam@gmail.com",
     description="Sequence-Space Jacobian Methods for Solving and Estimating Heterogeneous Agent Models",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,6 +24,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+
+    python_requires=">=3.8",
+    install_requires=read("requirements.txt").splitlines(),
 
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
